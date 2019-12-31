e22757ed142ad4761e84631985eef6dffac83287
moran <344330607@edufree.win>
2019-12-31 11:33:44 +0800

    1.增加跳转依赖

---
+++ base_module/src/main/java/com/moran/base/activity/ActivityConfig.kt
@@ -0,0 +1,22 @@
+package com.moran.base.activity
+
+
+/**
+  * @Package:
+  * @ClassName:      ActivityConfig
+  * @Description:    跳转配置
+  * @Author:         moran
+  * @CreateDate:     2019-12-09 10:01
+  * @UpdateUser:     更新者：moran
+  * @UpdateDate:     2019-12-09 10:01
+  * @UpdateRemark:   更新说明：
+  * @Version:        1.0
+ */
+internal object ActivityConfig {
+
+
+    val defaultParcelable = "ParcelAbleConfig"
+
+    val defaultBundle = "BundleConfig"
+
+}
